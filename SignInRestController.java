6b33aec6af767f64dc6ffc100760e05c576c46db
wrmyers <58116741+wrmyers@users.noreply.github.com>
2021-03-05 15:26:56 -0600

    Update SignInRestController.java

--- SignInRestController.java
+++ SignInRestController.java
@@ -1,65 +1,34 @@
 package edu.uark.registerapp.controllers;
 
-import java.util.UUID;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import edu.uark.registerapp.commands.products.ProductCreateCommand;
-import edu.uark.registerapp.commands.products.ProductDeleteCommand;
-import edu.uark.registerapp.commands.products.ProductUpdateCommand;
+import edu.uark.registerapp.commands.activeUsers.ActiveUserDeleteCommand;
+import edu.uark.registerapp.controllers.enums.ViewNames;
 import edu.uark.registerapp.models.api.ApiResponse;
-import edu.uark.registerapp.models.api.Product;
 
 @RestController
-@RequestMapping(value = "/api/product")
-public class ProductRestController {
-	@RequestMapping(value = "/", method = RequestMethod.POST)
-	public @ResponseBody ApiResponse createProduct(
-		@RequestBody final Product product
+@RequestMapping(value = "/api")
+public class SignInRestController extends BaseRestController {
+	@RequestMapping(value="/signOut", method = RequestMethod.DELETE)
+	public @ResponseBody ApiResponse removeActiveUser(
+		final HttpServletRequest request
 	) {
 
-		return this.productCreateCommand
-			.setApiProduct(product)
-			.execute();
-	}
-
-	@RequestMapping(value = "/{productId}", method = RequestMethod.PUT)
-	public @ResponseBody ApiResponse updateProduct(
-		@PathVariable final UUID productId,
-		@RequestBody final Product product
-	) {
-
-		return this.productUpdateCommand
-			.setProductId(productId)
-			.setApiProduct(product)
-			.execute();
-	}
-
-	@RequestMapping(value = "/{productId}", method = RequestMethod.DELETE)
-	public @ResponseBody ApiResponse deleteProduct(
-		@PathVariable final UUID productId
-	) {
-
-		this.productDeleteCommand
-			.setProductId(productId)
+		this.activeUserDeleteCommand
+			.setSessionKey(request.getSession().getId())
 			.execute();
 
-		return new ApiResponse();
+		return (new ApiResponse())
+			.setRedirectUrl(ViewNames.SIGN_IN.getRoute());
 	}
 
 	// Properties
 	@Autowired
-	private ProductCreateCommand productCreateCommand;
-	
-	@Autowired
-	private ProductDeleteCommand productDeleteCommand;
-	
-	@Autowired
-	private ProductUpdateCommand productUpdateCommand;
+	private ActiveUserDeleteCommand activeUserDeleteCommand;
 }
